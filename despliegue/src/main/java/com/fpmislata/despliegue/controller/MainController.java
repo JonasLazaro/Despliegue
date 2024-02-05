aee357307fe2b125a6234258c6fe671a159971e9
Jonás Lázaro Lasso <115450074+JonasLazaro@users.noreply.github.com>
2024-02-05 10:17:57 +0100

    Add files via upload

---
+++ despliegue/src/main/java/com/fpmislata/despliegue/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.fpmislata.despliegue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String holaMundo(){
+        return "holaMundo.html";
+    }
+}
